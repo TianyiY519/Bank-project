6ddc56bd04f37672bc643ce3e7cf14d280094ac5
TianyiY519 <70730404+TianyiY519@users.noreply.github.com>
2023-03-16 12:51:17 -0400

    Add files via upload

---
+++ bank/urls.py
@@ -0,0 +1,32 @@
+"""bank URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('', include('core.urls')),
+    path('user/', include(('UserAdmin.urls', 'UserAdmin'), namespace='UserAdmin')),
+    path('api/user/', include('UserAdmin.API.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('api/transactions/', include('transactions.API.urls')),
+    path('transactions/', include('transactions.urls', namespace='transactions')),
+    path('api-auth/', include('rest_framework.urls')),
+    #path('api/', include('Courses.API.urls')),
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
